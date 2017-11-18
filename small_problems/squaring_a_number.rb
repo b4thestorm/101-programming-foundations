fb4052f72b5662e774c58fa7b22b3171147e5019
Arnold <arnoldsander@gmail.com>
2017-11-18 00:47:27 -0500

    square a number using a loaded ruby file

---
+++ small_problems/squaring_a_number.rb
@@ -0,0 +1,16 @@
+load 'multiply.rb'
+
+
+# goal:  Using the multiply method from the "Multiplying Two Numbers" problem, write a 
+# method that computes the square of its argument (the square is the result of multiplying a number by itself).
+
+    def square(number)
+      square = multiply(number, number) 
+     end 
+  
+  puts = "Enter a number"
+  print "=>"
+  integer = gets.chomp.to_i
+    
+
+  puts square(integer)
