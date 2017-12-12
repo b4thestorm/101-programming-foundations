0e5b64081fa01b246f4f8697310304aa1784d29c
Arnold <arnoldsander@gmail.com>
2017-12-12 06:34:47 -0500

    practice 11

---
+++ small_problems/fibonacci.rb
@@ -0,0 +1,20 @@
+# goal: Write a method that calculates and returns the index of the first Fibonacci number
+#  that has the number of digits specified as an argument. (The first Fibonacci number has index 1.)
+
+
+logical layer
+  
+  
+
+
+syntactical layer 
+
+data = list.map {|x| r.location_data_from_ip(x.ip_address)}
+count = 0
+while count <= data.length 
+  list[count].city = data[count][:city]
+  list[count].province = data[count][:province]
+  list[count].country = data[count][:country]
+  list[count].save
+   count += 1
+end  
